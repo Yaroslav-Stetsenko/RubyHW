95d5709e6e36994fae5956f2c24cb311e285e949
kote1990 <kote25081990@gmail.com>
2021-11-21 06:13:48 -0800

    your message

---
+++ hello.rb
@@ -0,0 +1,13 @@
+puts 'Приветик, как тебя зовут'
+name1 = gets.chomp
+puts 'какое твое фамилие?'
+name2 = gets.chomp
+puts 'какое твое отчество?'
+name3 = gets.chomp
+puts 'Привет ' + name2 + ' ' + name1 + ' ' + name3
+
+
+puts name2 + ' ' + name1 + ' ' + name3 + 'у тебя есть любимое число'
+number1 = gets.chomp
+number2 = 1
+puts number1.to_i + number2 + 'this is better number'
