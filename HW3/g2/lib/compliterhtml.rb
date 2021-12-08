41a53960c4753acf572e8f50b8d69321f97f2a56
kote1990 <kote25081990@gmail.com>
2021-12-08 13:34:37 -0800

    up

--- HW3/g2/lib/compliterhtml.rb
+++ HW3/g2/lib/compliterhtml.rb
@@ -1,18 +1,23 @@
-class compliterhtml
-  def self.print(content, bypass_html, title, file = 'index.html')
-    html = <<~HTML
-      <!DOCTYPE html>
-      <html lang="en" dir="ltr">
-        <head>
-        <meta charset="utf-8">
-          <title>#{title}</title>
-        </head>
-        <body>
-          #{content}
-        </body>
-      </html>
-    HTML
+class GetHtml
+  def get(content, bypass_html = true, file_name = 'index.html')
+    markup = gets.chomp
+    markup = content.gsub!(/[<>]/, '') if bypass_html
+#    markup = content if bypass_html
+    
+    f = File.new("#{Dir.pwd}/#{file_name}", "w+")
+    f.puts "<!DOCTYPE HTML>"
+    f.puts '<html lang="en">'
+    f.puts "  <head>"
+    f.puts '    <meta charset="UTF-8">'
+    f.puts "    <title>Generated in getHtml</title>"
+    f.puts "  </head>"
+    f.puts "  <body>"
+    f.puts "    <p>#{markup}</p>"
+    f.puts "  </body>"
+    f.puts "</html>"
+    f.close
 
-    File.write(file, html)
   end
 end
+
+GetHtml.new.get("markup", true)
--- HW3/g2/tamagochi.rb
+++ HW3/g2/tamagochi.rb
@@ -1,6 +1,3 @@
-require 'compliterhtml'
-
-
 class Pet
   def initialize(name, kind)
     @name = name
@@ -22,13 +19,13 @@ class Pet
     @energy -= 5
     @wc += 1
     @eat -= 5
-    passageOfTime
+    pas_time
   end
 
   def want_poo
     @wc = 0
     @eat -= 1
-    passageOfTime
+    pas_time
   end
 
   def food
@@ -36,45 +33,30 @@ class Pet
     @happy += 3
     @love += 2
     @wc += 3
-    if @eat >= 50
-      p 'Вы точно хотите покормить ' + @name + '?
-    1 - Да
-    2 - Нет'
-      eat = gets.chomp.to_s
-      if eat == '1'
-        p 'Вы кормите ' + @name + '.'
-        @happy -= 1
-        @love -= 1
-      elsif eat == '2'
-        @happy += 2
-        @love += 2
-        p 'Хорошо, не перекармливайте питомца'
-      end
-    end
-    passageOfTime
+    pas_time
   end
 
   def sleep
     @sleepiness = 0
     @happy += 2
     @love += 2
-    passageOfTime
+    pas_time
   end
 
-  def bathe # искупаться
+  def bathe
     @health += 2
     @love = 10
     @energy -= 1
-    passageOfTime
+    pas_time
   end
 
-  def doctor # врач
+  def doctor
     @life += 5
     @health += 2
-    passageOfTime
+    pas_time
   end
 
-  def stroll # прогулка
+  def stroll
     @health -= 1
     @happy += 3
     @eat -= 2
@@ -83,10 +65,10 @@ class Pet
     @wc += 2
     @love += 5
     @energy -= 2
-    passageOfTime
+    pas_time
   end
 
-  def training # дрессировка
+  def training
     @happy += 3
     @eat -= 2
     @sleepiness -= 1
@@ -94,22 +76,22 @@ class Pet
     @wc += 2
     @love += 5
     @energy -= 2
-    passageOfTime
+    pas_time
   end
 
-  def scratch # почесать
+  def scratch
     @happy += 3
     @moods += 2
     @love += 5
-    passageOfTime
+    pas_time
   end
 
-  def independence # самостоятельность
+  def independence
     @love -= 2
     @moods -= 2
     @eat -= 2
     @happy -= 3
-    passageOfTime
+    pas_time
   end
 
   def help
@@ -127,44 +109,37 @@ class Pet
 
   private
 
-  def passageOfTime
+  def pas_time
     @life -= 1
     @eat -= rand(3)
     @moods -= rand(3)
     @wc += rand(3)
     @love -= 1
     @sleepiness += 1
-
     if @eat > 100
-      p 'Ваш ' + @name + " умирает от переедания\n"
+      p "Ваш #{@name} умирает от переедания."
       exit
     end
     if @life <= 0
-      p 'Ваш ' + @name + ' умирает от старостиn'
+      p "Ваш #{@name} умирает от старости."
       exit
     end
     if @sleepiness <= 0
-      p 'Ваш ' + @name + ' умирает от недосыпания'
+      p "Ваш #{@name} умирает от недосыпания."
       exit
     end
     if @moods <= 0
-      p 'Ваш ' + @name + ' покидает вас'
+      p "Ваш #{@name} покидает вас."
       exit
     end
     if @anger >= 20
-      p 'Ваш ' + @name + ' укусил вас и убежал'
+      p "Ваш #{@name} укусил вас и убежал."
       exit
     end
   end
-  if @wc == 10
-    p 'Злой ' + @name + ' писает на вас'
-    @wc = 0
-    @love -= 2
-    @anger += 5
-  end
 end
 
-print 'Введите название питомца: '
+p 'Введите название питомца:'
 name = gets.chomp.to_s
 print "Выберите вашего питомца:
     1 - Собака
@@ -172,25 +147,27 @@ print "Выберите вашего питомца:
     3 - Попугай
     4 - Дракон
     5 - Ничего не хочу\n"
-kind = gets.chomp.to_s
-pet = Pet.new name, kind
+com = gets.chomp.to_s
+pet = Pet.new name, @kind
 
-if kind == '1'
+case com
+when '1'
   @kind = 'Собака'
-elsif  kind == '2'
+when '2'
   @kind = 'Кошка'
-elsif  kind == '3'
+when '3'
   @kind = 'Попугай'
-elsif  kind == '4'
+when '4'
   @kind = 'Дракон'
-elsif  kind == '5'
-  p "Спасибо за игру\n"
+when '5'
+  @kind = 'Спасибо за игру'
   exit
+else
+  p 'Выберите команду от 1 до 5.'
 end
-p 'Ваш ' + @kind + ' с именем ' + name + ' родился.'
+p "Ваш #{@kind} с именем #{name} родился."
 p 'Поздравляем'
-
-p 'Что вы хотите сделать с ' + name + '?'
+p "Что вы хотите сделать с #{name}?"
 puts '    1 - Поиграть
     2 - Отвести в туалет
     3 - Покормить
@@ -207,35 +184,34 @@ until command == 'exit'
   case command
   when '1'
     pet.play
-    p 'Вы играети с ' + name
+    p "Вы играети с #{name}."
   when '2'
     pet.want_poo
-    p name + ' идет в туалет.'
+    p "#{name} идет в туалет."
   when '3'
     pet.food
-    p 'Вы кормите ' + name + ' и он счастлив.'
+    p "Вы кормите #{name} и он счастлив."
   when '4'
     pet.sleep
-    p 'Вы укладываете ' + name + ' спать.'
-    p 'Через некоторое время ' + name + ' просыпается.'
+    p "Вы укладываете #{name} спать."
   when '5'
     pet.bathe
-    p 'Ваш питомец ' + name + ' теперь чистенький.'
+    p "Ваш питомец #{name} теперь чистенький."
   when '6'
     pet.doctor
-    p 'Ваш питомец ' + name + ' стал здоровее.'
+    p "Ваш питомец #{name} стал здоровее."
   when '7'
     pet.stroll
-    p 'Вы прогулялись ' + name + ', он стал счастливее.'
+    p "Вы прогулялись c #{name}, он стал счастливее."
   when '8'
     pet.training
-    p 'Вы дресируете ' + name + '.'
+    p "Вы дресируете #{name}."
   when '9'
     pet.scratch
-    p 'Вы почесали ' + name + '.'
+    p "Вы почесали #{name}."
   when '10'
     pet.independence
-    p 'Ваш ' + @kind + ' развлекает себя сам.'
+    p "Ваш #{@kind} развлекает себя сам."
   when '11'
     pet.help
   else
